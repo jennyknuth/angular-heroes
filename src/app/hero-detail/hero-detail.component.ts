2969dd4aed9d99957a1a052d243f3886f72f8ea3
jknuth <knuth@macl3789.lasp.colorado.edu>
2018-07-10 12:01:17 -0600

    add goBack method using location

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,11 @@ export class HeroDetailComponent implements OnInit {
         const id = +this.route.snapshot.paramMap.get('id');
         this.heroService.getHero(id)
             .subscribe(hero => this.hero = hero);
-}
+
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
 
 }
