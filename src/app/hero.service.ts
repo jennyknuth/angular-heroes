19d0517498e98cced86bd5a435bc84201a72ea19
jknuth <knuth@macl3789.lasp.colorado.edu>
2018-07-16 16:40:47 -0600

    remove mock backend and use inMemoryWebApiModule and http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,29 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { MessageService } from './message.service';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeroService {
 
+    private heroesUrl = 'api/heroes';  // URL to web api
+
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService
+    ) {}
+
     getHeroes(): Observable<Hero[]> {
         // TODO: send the message _after_ fetching the heroes
-        this.messageService.add('HeroService: fetched heroes');
-        return of(HEROES);
+        return this.http.get<Hero[]>(this.heroesUrl);
     }
 
     getHero(id: number): Observable<Hero> {
         // TODO: send the message _after_ fetching the hero
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(HEROES.find(hero => hero.id === id));
+        return this.http.get<Hero>(this.heroesUrl);
     }
 
-
-    constructor(private messageService: MessageService) { }
-
+    /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+        this.messageService.add('HeroService: ' + message);
+    }
 }
